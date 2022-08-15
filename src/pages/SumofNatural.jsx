83b5a304d439b3c4db3ec1c1dfcde8c3d64bec3e
AriesMolinar22 <webdev652@gmail.com>
2022-08-15 19:33:31 +0800

    final commit

--- src/pages/SumofNatural.jsx
+++ src/pages/SumofNatural.jsx
@@ -1,49 +1,39 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 
-function SumofNatural() {
+export default function SumOfNatural() {
   const [input, setInput] = useState("");
 
-
-  const getNaturalNumber = () => {
-    //check if letter or null
+  const getSumOfNaturalNumbers = () => {
     if (isNaN(input) || !input) {
-      return "Please enter a numer"
+      return "Please enter a number";
     }
 
-    //creating sum of natural number
-    let sum = 0;
-    for (let i = 1; i <= input; i++)
-    sum = sum + i;
+    let total = 0;
+    for (let i = 1; i <= input; i++) {
+      total = total + i;
+    }
 
-    return sum;
-}
+    return total;
+  };
+
   return (
     <div className="container p-5">
       <div className="row">
-        <div className="col-md-5">
+        <div className="col-12 w-50 pb-5">
           <input
             type="text"
             className="w-100 lead"
             placeholder="Input"
             value={input}
-            onChange={(e) => setInput(e.target.value)}
+            onChange={(event) => {
+              setInput(event.target.value);
+            }}
           />
         </div>
-        <div className="col-md-5">
-          <textarea
-            name="text"
-            id="text"
-            cols="30"
-            rows="10"
-            readOnly={true}
-            placeholder="Output"
-            value={getNaturalNumber()}
-          />
-        </div>
+        <h1 className="col-12 w-100 text-primary">
+          {getSumOfNaturalNumbers()}
+        </h1>
       </div>
     </div>
   );
 }
-
-export default SumofNatural;
